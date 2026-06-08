4e0eaceb0415f8e8790a9f959c65f2b22908158d
Desenvolvedora Salesforce Júnior <111286589+aGaAmorim@users.noreply.github.com>
2026-06-08 11:56:27 -0300

    Update convite.js

--- js/convite.js
+++ js/convite.js
@@ -1,14 +1,50 @@
-function validarCodigo(event) {
+const API_URL =
+  'https://script.google.com/macros/s/AKfycbw3CbALrF6YwnXNiHs-UCnZzZeCbDFi9aihl7y4F6irkD7b9A7ctrsb2Yy8HVNaLAi7/exec';
+
+async function validarCodigo(event) {
   event.preventDefault();
-  
-  const codigo = document.getElementById("codigo").value.trim().toUpperCase();
-  console.log("Código digitado: ", codigo);
-  if (convites[codigo]) {
-    localStorage.setItem("codigoConvite", codigo);
-    window.location.href = "convidados.html";
-  } else {
-    mostrarErro("Código inválido 😕");
-  }  
+
+  const codigo = document
+    .getElementById("codigo")
+    .value
+    .trim()
+    .toUpperCase();
+
+  try {
+
+    const response = await fetch(
+      `${API_URL}?action=buscar&codigo=${encodeURIComponent(codigo)}`
+    );
+
+    const data = await response.json();
+
+    if (data.success) {
+
+      localStorage.setItem(
+        "codigoConvite",
+        codigo
+      );
+
+      window.location.href =
+        "convidados.html";
+
+    } else {
+
+      mostrarErro(
+        "Código inválido 😕"
+      );
+
+    }
+
+  } catch (error) {
+
+    console.error(error);
+
+    mostrarErro(
+      "Erro ao consultar convite 😕"
+    );
+
+  }
 }
 
 function mostrarErro(msg) {
