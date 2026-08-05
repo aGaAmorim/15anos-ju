eac278139ae90d484bae735da9ef75c2643b9dcc
Desenvolvedora Salesforce Júnior <111286589+aGaAmorim@users.noreply.github.com>
2026-08-05 12:07:56 -0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-const dataFesta = new Date("2026-12-11T19:00:00").getTime();
+/*const dataFesta = new Date("2026-12-11T19:00:00").getTime();
 
 function atualizarContagem() {
 
@@ -14,6 +14,61 @@ function atualizarContagem() {
   document.getElementById("horas").innerText = horas;
   document.getElementById("minutos").innerText = minutos;
   document.getElementById("segundos").innerText = segundos;
+}
+
+setInterval(atualizarContagem, 1000);
+atualizarContagem();
+*/
+
+const dataFesta = new Date(2026, 11, 11, 19, 0, 0);
+
+function atualizarContagem() {
+
+    const agora = new Date();
+
+    let anos = dataFesta.getFullYear() - agora.getFullYear();
+    let meses = dataFesta.getMonth() - agora.getMonth();
+    let dias = dataFesta.getDate() - agora.getDate();
+    let horas = dataFesta.getHours() - agora.getHours();
+    let minutos = dataFesta.getMinutes() - agora.getMinutes();
+    let segundos = dataFesta.getSeconds() - agora.getSeconds();
+
+    if (segundos < 0) {
+        segundos += 60;
+        minutos--;
+    }
+
+    if (minutos < 0) {
+        minutos += 60;
+        horas--;
+    }
+
+    if (horas < 0) {
+        horas += 24;
+        dias--;
+    }
+
+    if (dias < 0) {
+        const ultimoDiaMesAnterior = new Date(
+            dataFesta.getFullYear(),
+            dataFesta.getMonth(),
+            0
+        ).getDate();
+
+        dias += ultimoDiaMesAnterior;
+        meses--;
+    }
+
+    if (meses < 0) {
+        meses += 12;
+        anos--;
+    }
+
+    document.getElementById("meses").innerText = anos * 12 + meses;
+    document.getElementById("dias").innerText = dias;
+    document.getElementById("horas").innerText = horas;
+    document.getElementById("minutos").innerText = minutos;
+    document.getElementById("segundos").innerText = segundos;
 }
 
 setInterval(atualizarContagem, 1000);
